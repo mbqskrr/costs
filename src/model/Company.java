6d5a212a3c1ae61f1f4b9e05d58b7cb2d71c8d5f
Manuel Balanta <mbalantaq@gmail.com>
2020-05-14 01:21:18 -0500

    Error para leer archivo

--- src/model/Company.java
+++ src/model/Company.java
@@ -1,16 +1,24 @@
 package model;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 
 public class Company {
 	
+	public final static String PATH = "data/employees.txt";
 	private String name;
 	private String nit;
-	private ArrayList<Employee> e;
+	private ArrayList<Employee> employeeArray;
 	
 	public Company(String name, String nit) {
 		this.name = name;
 		this.nit = nit;
+		employeeArray = new ArrayList<Employee>();
 	}
 	
 	public Company() {
@@ -25,20 +33,53 @@ public class Company {
 		this.name = name;
 	}
 	
-	public ArrayList<Employee> getE() {
-		return e;
-	}
-	
-	public void setE(ArrayList<Employee> e) {
-		this.e = e;
-	}
-	
 	public String getNit() {
 		return nit;
 	}
 	
 	public void setNit(String nit) {
 		this.nit = nit;
+	}
+	
+	public ArrayList<Employee> getEmployeeArray() {
+		return employeeArray;
+	}
+
+	public void setEmployeeArray(ArrayList<Employee> employeeArray) {
+		this.employeeArray = employeeArray;
+	}
+
+	public void importReport(String sep) throws IOException {
+		File f = new File(PATH);
+		FileReader fr = new FileReader(f);
+		BufferedReader br = new BufferedReader(fr);
+		String line = br.readLine();
+		while (line!=null) {
+			String[] parts = line.split(sep);
+			String name = parts[0];
+			int salary = Integer.parseInt(parts[1]);
+			String id = parts[2];
+			String charge = parts[3];
+			String dependency = parts[4];
+			String doa = parts[5];
+			Employee e = new Employee(name, salary, id, charge, dependency, doa);
+			employeeArray.add(e);
+			System.out.println(employeeArray.get(0)+" no hay error");
+			line = br.readLine();
+		}
+		fr.close();
+		br.close();
+	}
+	
+	public void exportReport() throws FileNotFoundException{
+		String msg = "";
+		for (int i = 0; i < employeeArray.size()-1; i++) {
+			Employee current = employeeArray.get(i);
+			msg+=current.getName()+";"+current.getSalary()+";"+current.getId()+";"+current.getCharge()+";"+current.getDependency()+";"+current.getDateOfAdmission()+"\n";
+		}
+		PrintWriter pw = new PrintWriter(new File(PATH));
+		pw.print(msg);
+		pw.close();
 	}
 	
 	
--- src/ui/PayrollController.java
+++ src/ui/PayrollController.java
@@ -1,5 +1,127 @@
 package ui;
 
+import java.io.IOException;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.Tab;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextField;
+import model.Company;
+import model.Employee;
+
 public class PayrollController {
+	
+	@FXML
+    private Tab tabCompany;
+
+	@FXML
+	private TextField txtCompany;
+
+	@FXML
+	private TextField txtNIT;
+
+	@FXML
+	private Button btnNext;
+
+	@FXML
+	private Button btnSelect;
+
+	@FXML
+	private Button btnSave;
+
+	@FXML
+	private Button btnEdit;
+
+	@FXML
+	private Button btnDelete;
+
+	@FXML
+	private TableView<Employee> tableView;
+
+	@FXML
+	private TableColumn<Employee, String> nameColumn;
+
+	@FXML
+	private TableColumn<Employee, Integer> salaryColumn;
+
+	@FXML
+	private TableColumn<Employee, String> idColumn;
+
+	@FXML
+	private TableColumn<Employee, String> chargeColumn;
+
+	@FXML
+	private TableColumn<Employee, String> depencyColumn;
+
+	@FXML
+	private TableColumn<Employee, String> doaColumn;
+
+	@FXML
+	private DatePicker jDate;
+
+	@FXML
+	private Label lblCompanyName;
+
+	@FXML
+	private TextField txtEmployee;
+
+	@FXML
+	private TextField txtCategory;
+
+	@FXML
+	private TextField txtPeriod;
+
+	@FXML
+	private TextField txtCuenta;
+
+	@FXML
+	private TextField txtCobro;
+
+	@FXML
+	private TextField txtEarned;
+
+	@FXML
+	private Tab tabEmployee;
+
+	@FXML
+	private Tab tabPayroll;
+
+	private Company c;
+
+	public void initialize() {
+		c = new Company();
+	}
+
+	@FXML
+	void showTabs(ActionEvent event) {
+		c.setName(txtCompany.getText());
+		c.setNit(txtNIT.getText());
+		lblCompanyName.setText(c.getName());
+		tabEmployee.setDisable(false);
+		tabPayroll.setDisable(false);
+		tabCompany.setDisable(true);
+		try {
+			c.importReport(";");
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		tableView.setItems(getEmployees());
+		System.out.println(c.getEmployeeArray().get(0));
+	}
+	
+	public ObservableList<Employee> getEmployees(){
+		ObservableList<Employee> emp = FXCollections.observableArrayList();
+		for (int i = 0; i < c.getEmployeeArray().size()-1; i++) {
+			emp.add(c.getEmployeeArray().get(i));
+		}
+		return emp;
+	}
 
 }
